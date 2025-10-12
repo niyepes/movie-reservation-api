bb3b55961b828a02b4faec3f46c55b203f1a2869
Nicolas <nayepesc@udistrital.edu.co>
2025-10-12 16:12:55 -0500

    Se agregó la constante que toma el resultado de intentar conectarse a la base de datos de Mongo

---
+++ src/db/mongo.ts
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+import {ENV} from '../config/env.js';
+
+export const connectToDatabase = async () => {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(ENV.MONGODB_URI);
+    console.log('Connected to MongoDB');
+}
