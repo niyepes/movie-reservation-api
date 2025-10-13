342b5981841c8f5c8ce12e7981ec983381bf5dc5
Nicolas <nayepesc@udistrital.edu.co>
2025-10-13 10:25:26 -0500

    Se agregó el archivo principal para correr la aplicación

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import {connectToDatabase} from './db/mongo.js';
+import reservationRouter from './routes/reservationRoutes.js';
+import { errorHandler } from './middlewares/error';
+import {ENV} from './config/env.js';
+
+async function bootstrap() {
+    await connectToDatabase();
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+
+    app.use('/api/reservations', reservationRouter);
+    app.use(errorHandler);
+
+    app.listen(ENV.PORT, () => {
+        console.log(`Server is running on port ${ENV.PORT}`);
+    });
+}
+
+bootstrap().catch(err => {
+    console.error('Failed to start server:', err);
+    process.exit(1);
+});
+
